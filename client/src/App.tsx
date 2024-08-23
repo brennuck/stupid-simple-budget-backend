836eff6b8d5d9b3e1a4a9bf6aa321ab895c2e6e2
Brennon Nuckols <47544816+brennuck@users.noreply.github.com>
2024-08-22 20:12:10 -0600

    add dark mode

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,10 +4,17 @@ import { AccountsTable } from "./components/AccountsTable";
 import { Col, Row, Skeleton, Typography } from "antd";
 import { TransactionsTable } from "./components/TransactionsTable";
 import { API_URL } from "./config";
+import { ConfigProvider, theme } from "antd";
+import "./ThemeStyle.css";
 
 function App() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
     const [accountsData, setAccountsData] = useState<any>(null);
     const [transactionsData, setTransactionsData] = useState<any>(null);
+
+    const toggleTheme = () => {
+        setIsDarkMode(!isDarkMode);
+    };
 
     const fetchData = () => {
         const url = API_URL ? API_URL : "";
@@ -42,18 +49,27 @@ function App() {
     }
 
     return (
-        <>
-            <Row>
-                <Col offset={1}>
-                    <Typography.Title level={2}>Stupid Simple Budget</Typography.Title>
-                </Col>
-            </Row>
-            <Row>
-                <Col offset={1} span={22}>
-                    {children}
-                </Col>
-            </Row>
-        </>
+        <ConfigProvider
+            theme={{
+                algorithm: isDarkMode ? theme.darkAlgorithm : theme.defaultAlgorithm,
+                token: { colorPrimary: "#A28089" },
+            }}
+        >
+            <div className={isDarkMode ? "dark-mode" : "light-mode"}>
+                <Row>
+                    <Col offset={1}>
+                        <Typography.Title level={2} onClick={toggleTheme} style={{ cursor: "pointer" }}>
+                            Stupid Simple Budget
+                        </Typography.Title>
+                    </Col>
+                </Row>
+                <Row>
+                    <Col offset={1} span={22}>
+                        {children}
+                    </Col>
+                </Row>
+            </div>
+        </ConfigProvider>
     );
 }
 
