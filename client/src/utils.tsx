117b17be6e1ee1f67bc2b2eeb983ce223a451837
Brennon Nuckols <47544816+brennuck@users.noreply.github.com>
2024-10-28 21:25:32 -0600

    remove v1 client

--- client/src/utils.tsx
+++
@@ -1,6 +0,0 @@
-let USDollar = new Intl.NumberFormat("en-US", {
-    style: "currency",
-    currency: "USD",
-});
-
-export const formatCurrency = (amount: number) => USDollar.format(amount);
--- index.tsx
+++ index.tsx
@@ -31,51 +31,51 @@ app.use(
     })
 );
 
-const performWeeklyDeposits = async () => {
-    const accounts = await getWeeklyDeposits();
-    const today = new Date();
+// const performWeeklyDeposits = async () => {
+//     const accounts = await getWeeklyDeposits();
+//     const today = new Date();
 
-    for (const account of accounts) {
-        try {
-            const startDate = new Date(account.insert_start_date);
-            let shouldDeposit = false;
+//     for (const account of accounts) {
+//         try {
+//             const startDate = new Date(account.insert_start_date);
+//             let shouldDeposit = false;
 
-            switch (account.insert_frequency) {
-                case "weekly":
-                    shouldDeposit = (today.getTime() - startDate.getTime()) % (7 * 24 * 60 * 60 * 1000) === 0;
-                    break;
-                case "bi-weekly":
-                    shouldDeposit = (today.getTime() - startDate.getTime()) % (14 * 24 * 60 * 60 * 1000) === 0;
-                    break;
-                case "monthly":
-                    shouldDeposit = today.getDate() === startDate.getDate();
-                    break;
-            }
+//             switch (account.insert_frequency) {
+//                 case "weekly":
+//                     shouldDeposit = (today.getTime() - startDate.getTime()) % (7 * 24 * 60 * 60 * 1000) === 0;
+//                     break;
+//                 case "bi-weekly":
+//                     shouldDeposit = (today.getTime() - startDate.getTime()) % (14 * 24 * 60 * 60 * 1000) === 0;
+//                     break;
+//                 case "monthly":
+//                     shouldDeposit = today.getDate() === startDate.getDate();
+//                     break;
+//             }
 
-            if (shouldDeposit) {
-                const transaction = await createDepositTransaction({
-                    amount: account.insert_amount,
-                    description: `Automatic ${account.insert_frequency} deposit`,
-                    date: today.toISOString(),
-                    account_id: account.id,
-                });
-                await depositToAccount(transaction.to_account_id, transaction.amount);
-                console.log(
-                    `Automatic ${account.insert_frequency} deposit of ${account.insert_amount} to account ${account.id} successful`
-                );
-            }
-        } catch (error) {
-            console.error(`Error performing automatic deposit to account ${account.id}:`, error);
-        }
-    }
-};
+//             if (shouldDeposit) {
+//                 const transaction = await createDepositTransaction({
+//                     amount: account.insert_amount,
+//                     description: `Automatic ${account.insert_frequency} deposit`,
+//                     date: today.toISOString(),
+//                     account_id: account.id,
+//                 });
+//                 await depositToAccount(transaction.to_account_id, transaction.amount);
+//                 console.log(
+//                     `Automatic ${account.insert_frequency} deposit of ${account.insert_amount} to account ${account.id} successful`
+//                 );
+//             }
+//         } catch (error) {
+//             console.error(`Error performing automatic deposit to account ${account.id}:`, error);
+//         }
+//     }
+// };
 
-const getWeeklyDeposits = async () => {
-    const result = await pool.query(
-        "SELECT id, insert_frequency, insert_amount, insert_start_date FROM accounts where insert_frequency is not null and insert_amount is not null and insert_start_date is not null;"
-    );
-    return result.rows;
-};
+// const getWeeklyDeposits = async () => {
+//     const result = await pool.query(
+//         "SELECT id, insert_frequency, insert_amount, insert_start_date FROM accounts where insert_frequency is not null and insert_amount is not null and insert_start_date is not null;"
+//     );
+//     return result.rows;
+// };
 
 const getAccounts = async () => {
     const result = await pool.query("SELECT * FROM accounts order by id asc;");
@@ -94,66 +94,6 @@ const createAccount = async (account) => {
 const getTransactions = async () => {
     const result = await pool.query("SELECT * FROM transactions order by created_at desc;");
     return result.rows;
-};
-
-const createWithdrawalTransaction = async (transaction) => {
-    const result = await pool.query(
-        "INSERT INTO transactions (amount, description, date, from_account_id) VALUES ($1, $2, $3, $4) RETURNING *;",
-        [`-${transaction.amount}`, transaction.description, transaction.date, transaction.account_id]
-    );
-    return result.rows[0];
-};
-
-const createDepositTransaction = async (transaction) => {
-    const result = await pool.query(
-        "INSERT INTO transactions (amount, description, date, to_account_id) VALUES ($1, $2, $3, $4) RETURNING *;",
-        [transaction.amount, transaction.description, transaction.date, transaction.account_id]
-    );
-    return result.rows[0];
-};
-
-const withdrawFromAccount = async (account_id, amount) => {
-    const client = await pool.connect();
-    try {
-        await client.query("BEGIN");
-
-        const result = await client.query("UPDATE accounts SET balance = balance + $1 WHERE id = $2 RETURNING *;", [
-            amount,
-            account_id,
-        ]);
-
-        await client.query("COMMIT");
-
-        return result.rows[0];
-    } catch (e) {
-        await client.query("ROLLBACK");
-        throw e;
-    } finally {
-        client.release();
-    }
-};
-
-const depositToAccount = async (account_id, amount) => {
-    const client = await pool.connect();
-    try {
-        await client.query("BEGIN");
-        const result = await client.query("UPDATE accounts SET balance = balance + $1 WHERE id = $2 RETURNING *;", [
-            amount,
-            account_id,
-        ]);
-
-        if (account_id !== 1 && account_id !== 9) {
-            await client.query("UPDATE accounts SET balance = balance - $1 WHERE name = 'savings';", [amount]);
-        }
-
-        await client.query("COMMIT");
-        return result.rows[0];
-    } catch (e) {
-        await client.query("ROLLBACK");
-        throw e;
-    } finally {
-        client.release();
-    }
 };
 
 const createTransaction = async (transaction) => {
@@ -237,10 +177,10 @@ app.post("/transaction", async (req, res) => {
 });
 
 // Schedule the deposits to run daily at midnight
-cron.schedule("0 0 * * *", () => {
-    console.log("Running daily deposit check");
-    performWeeklyDeposits();
-});
+// cron.schedule("0 0 * * *", () => {
+//     console.log("Running daily deposit check");
+//     performWeeklyDeposits();
+// });
 
 // Add these functions after the existing ones
 
@@ -348,8 +288,6 @@ const insertData = async (data) => {
         throw error;
     }
 };
-
-// Add these new routes after the existing ones
 
 app.get("/download-data", async (req, res) => {
     try {
